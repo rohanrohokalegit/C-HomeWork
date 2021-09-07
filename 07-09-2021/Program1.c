c2c2a091fbc1b766da59b8db9db66c8a5589b21a
Rohan Rohokale <rohanrohokalegit@gmail.com>
2021-09-07 08:47:23 +0530

    program 1

--- 07-09-2021/Program1.c
+++ 07-09-2021/Program1.c
@@ -1,6 +1,19 @@
+//display reverse number on screen
+//INput: 5
+//output: 5 4 3 2 1
+
 #include <stdio.h>
+void revDisplay();
 int main()
 {
-    Printf("Jay Ganesha");
+    revDisplay();
     return 0;
+}
+void revDisplay()
+{
+    int i = 0;
+    for (i = 5; i >= 1; i--)
+    {
+        printf("%d\n", i);
+    }
 }
